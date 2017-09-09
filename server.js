5bfd4144d3685609ea361abfa21450a388a91542
Jerry Moore <jmoor6@monsanto.com>
2017-09-09 11:51:56 -0500

    - Added CORS headers to allow cross-origin requests from UI (since they both run on different ports.)

--- server.js
+++ server.js
@@ -12,17 +12,28 @@ mongoose.connect(mongoUrl, (err) => {
     if (err) {
         console.error('Error!  Could not connect to MongoDB!', err);
         process.exit(0);
-    } else {
-        console.log('Connected to Mongo');
+    }
 
-        app.use((req, res, next) =>{
-            console.info("userId=" + req.userId + ", Method=" + req.method + ", Request=" + req.protocol + "://" + req.get('host') + req.originalUrl);
+    console.log('Connected to Mongo');
+
+    if (process.env.NODE_ENV !== 'production') {
+        console.log('setting up CORS middle-ware');
+        app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Headers', 'origin, content-type, accept, Access-Control-Allow-Origin');
+            res.header('Access-Control-Allow-Origin', req.header('origin'));
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+            res.header('Access-Control-Allow-Credentials', true);
             next();
         });
-
-        app.use(baseUrl, routes);
-        app.listen(port, function() {
-            console.log('MBU mock API listening on port: ', port);
-        });
     }
+
+    app.use((req, res, next) =>{
+        console.info("userId=" + req.userId + ", Method=" + req.method + ", Request=" + req.protocol + "://" + req.get('host') + req.originalUrl);
+        next();
+    });
+
+    app.use(baseUrl, routes);
+    app.listen(port, function() {
+        console.log('MBU mock API listening on port: ', port);
+    });
 });
